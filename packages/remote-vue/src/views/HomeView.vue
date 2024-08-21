3f603cbc921a018b99999fe0253f7bbcfe4e7f08
wanghaibo <1056140533@qq.com>
2024-08-21 16:09:08 +0800

    chore: 暂存

---
+++ packages/remote-vue/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-container class="wrap">
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+.wrap {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
